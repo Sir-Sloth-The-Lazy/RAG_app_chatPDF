fd4c5574eb0ba25afcb1208fb4b8aa356d369969
Jeevant  Prakhar  Singh <anupamasinghsrinet1976@gmail.com>
2025-04-25 11:33:52 +0530

    Fix: remove nested Git repo from client

---
+++ README.md
@@ -0,0 +1,3 @@
+# RAG_Chat-with-PDF
+##  1. System Design
+![](diagram-export-4-24-2025-5_18_34-PM.png)
---
+++ client/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-slate-900 to-slate-700">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  );
+}
